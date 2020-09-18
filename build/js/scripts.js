ae2804a81d26b62d1e166446a3a5796b40d15b22
matwey-project <xzzmanzzx@gmail.com>
2020-09-18 16:52:29 +0300

    Calculate Fix

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -9,21 +9,38 @@ null==d?void 0:d))},attrHooks:{type:{set:function(a,b){if(!o.radioValue&&"radio"
 var dataCalendar = [{
         'date-day': '2020,9,17',
         'date-info': [
-            ['18: 25', 'Курсы по мод 1'],
-            ['18: 30', 'Курсы по мод 2'],
-            ['18: 35', 'Курсы по мод 3'],
+            '<span>12:25</span> Курсы по мод 1.1',
+            '<span>12:30</span> Курсы по мод 1.2',
+            '<span>12:35</span> Курсы по мод 1.3'
         ]
     },
     {
-        'date-day': '2020,9,3',
+        'date-day': '2020,9,5',
         'date-info': [
-            ['12: 25', 'Курсы по мод 1'],
-            ['12: 30', 'Курсы по мод 2'],
-            ['12: 35', 'Курсы по мод 3'],
+            '<span>16:25</span> Курсы по мод 2.1',
+            '<span>16:30</span> Курсы по мод 2.2',
+            '<span>16:35</span> Курсы по мод 2.3',
+            '<span>17:35</span> Курсы по мод 2.4',
+            '<span>10:35</span> Курсы по мод 2.5',
+        ]
+    },
+    {
+        'date-day': '2020,9,20',
+        'date-info': [
+            '<span>18:25</span> Курсы по мод 3.1',
+            '<span>18:30</span> Курсы по мод 3.2',
+            '<span>18:35</span> Курсы по мод 3.3'
+        ]
+    },
+    {
+        'date-day': '2020,9,18',
+        'date-info': [
+            '<span>18:25</span> Курсы по мод 3.1',
+            '<span>18:30</span> Курсы по мод 3.2',
+            '<span>18:35</span> Курсы по мод 3.3'
         ]
     },
 ];
-// console.log(dataCalendar[0]['date-day'])
 
 function Calendar2(id, year, month) {
     var Dlast = new Date(year, month + 1, 0).getDate(),
@@ -39,40 +56,9 @@ function Calendar2(id, year, month) {
     document.querySelector('#' + id).addEventListener('click', function(e) {
         var target = e.target;
 
-
-        // console.log(new Date(dataCalendar[0]['date-day']))
-
         if (!target.children[0].innerText) {
             console.log('good')
         }
-
-        for (var i = 0; i < dataCalendar.length; i++) {
-            findDate(i);
-        }
-
-        function findDate(data) {
-            for (var i = 1; i < Dlast; i++) {
-                massDays = new Date(dataCalendar[data]['date-day']);
-                findDays = new Date(new Date().getFullYear(), new Date().getMonth(), i);
-                // console.log(massDays - findDays == 0)
-                if (massDays - findDays == 0) {
-                    console.log(i)
-                }
-            }
-        }
-
-        // console.log(target.children.length)
-        // nowDay.setDate(+target.children[0].innerText)
-        // console.log(nowDay.getDate()) // День при клике
-        // console.log(target.children[0].innerText)
-        // console.log(new Date().getFullYear(), new Date().getMonth())
-        // console.log(new Date(D.getFullYear(), D.getMonth(), 1).getDay())
-        // console.log(new Date().getDate())
-        // console.log(new Date().getFullYear())
-        // console.log(new Date().getMonth())
-        // console.log(new Date(new Date().getFullYear(), new Date().getMonth(), +target.children[0].innerText))
-        // console.log(new Date('2020,9,14'))
-        // console.log(new Date(dataCalendar['date-day']))
     });
 
     if (DNfirst != 0) {
@@ -83,65 +69,30 @@ function Calendar2(id, year, month) {
     for (var i = 1; i <= Dlast; i++) {
         var sliceOb = null,
             timeDate,
-            infoDate;
+            infoDate = "";
         findDays = new Date(new Date().getFullYear(), new Date().getMonth(), i);
-
-
-        if (i == new Date().getDate() && D.getFullYear() == new Date().getFullYear() && D.getMonth() == new Date().getMonth()) {
-            // calendar += '<td class="today"><span class="day-default">' + i;
-            for (var j = 0; j < dataCalendar.length; j++) {
-                timeDate = dataCalendar[j]['date-time'];
-                infoDate = dataCalendar[j]['date-info'];
-                // console.log(dataCalendar[j]['date-info'][0][0])
-                for (var inc = 0; inc < dataCalendar[j]['date-info'][0].length; inc++) {
-                    console.log(dataCalendar[j]['date-info'][0][inc])
-                }
-                massDays = new Date(dataCalendar[j]['date-day']);
-                if (massDays.getTime() == findDays.getTime() && D.getFullYear() == new Date().getFullYear() && D.getMonth() == new Date().getMonth()) {
-                    sliceOb = true;
+        for (var j = 0; j < dataCalendar.length; j++) {
+            massDays = new Date(dataCalendar[j]['date-day']);
+            if (massDays.getTime() == findDays.getTime() && D.getFullYear() == new Date().getFullYear() && D.getMonth() == new Date().getMonth()) {
+                sliceOb = true;
+                for (var inc = 0; inc < dataCalendar[j]['date-info'].length; inc++) {
+                    infoDate += '<li>' + dataCalendar[j]['date-info'][inc] + '</li>';
                 }
             }
+        }
 
+        if (i == new Date().getDate() && D.getFullYear() == new Date().getFullYear() && D.getMonth() == new Date().getMonth()) {
             if (sliceOb) {
-                calendar += '<td class="today"><div class="date-wrapper">' + timeDate + '<br>' + infoDate + '</div><span class="day-default">' + i;
+                calendar += '<td class="today"><div class="date-wrapper"><ul class="date-list">' + infoDate + '</div><span class="day-default">' + i;
             } else {
                 calendar += '<td class="today"><span class="day-default">' + i;
             }
         } else {
-
-            for (var j = 0; j < dataCalendar.length; j++) {
-                timeDate = dataCalendar[j]['date-time'];
-                infoDate = dataCalendar[j]['date-info'];
-                massDays = new Date(dataCalendar[j]['date-day']);
-                if (massDays.getTime() == findDays.getTime() && D.getFullYear() == new Date().getFullYear() && D.getMonth() == new Date().getMonth()) {
-                    sliceOb = true;
-                }
-            }
-
             if (sliceOb) {
-                calendar += '<td><div class="date-wrapper"><ul><li>' + timeDate + infoDate + '</div><span class="day-default">' + i;
+                calendar += '<td><div class="date-wrapper"><ul class="date-list">' + infoDate + '</div><span class="day-default">' + i;
             } else {
                 calendar += '<td><span class="day-default">' + i;
             }
-
-            // console.log(i)
-            // if (dataCalendar.length) {
-            //     for (var j = 0; j < dataCalendar.length; j++) {
-            //         // findDate(i);
-            //         massDays = new Date(dataCalendar[j]['date-day']);
-            //         // findDays = new Date(new Date().getFullYear(), new Date().getMonth(), i);
-            //         console.log(dataCalendar[j]['date-day'])
-            //         if (massDays - findDays == 0) {
-            //             calendar += '<td><div class="sdsdf"><span class="day-default">' + i;
-            //         }
-            //     }
-            //     // console.log(i + " - " + (massDays - findDays == 0))
-
-            //     // calendar += '<td><div class="sdsdf"><span class="day-default">' + i;
-            // } else {
-            //     calendar += '<td><span class="day-default">' + i;
-            // }
-
         }
         if (new Date(D.getFullYear(), D.getMonth(), i).getDay() == 0) {
             calendar += '<tr>';
